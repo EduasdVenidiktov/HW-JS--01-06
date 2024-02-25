8374bfe727c52f49dc87df6d98f40c732a831f35
EduardVenidiktov <edven7777777@gmail.com>
2024-02-26 00:02:25 +0200

    HW-01 Task-3

--- js/task-3.js
+++ js/task-3.js
@@ -1,26 +1,39 @@
-'use strict';
+"use strict";
 
-const profile = {
-    username: "Jacob",
-    playTime: 300,
-    
-    changeUsername(newName) {
-        profile.username = newName;
-    },
+// ======== ДЗ-1================================================
 
-    updatePlayTime(hours) {
-        this.playTime += hours;
-    },
+function getElementWidth(content, padding, border) {
+  const a = parseFloat(content);
+  const b = parseFloat(padding) * 2;
+  const c = parseFloat(border) * 2;
+  return a + b + c;
+}
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
 
-    getInfo(){
-return `${this.username} has ${profile.playTime} active hours!`;
-    },    
-};
+// =================================================================================
+// const profile = {
+//     username: "Jacob",
+//     playTime: 300,
 
-console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+//     changeUsername(newName) {
+//         profile.username = newName;
+//     },
 
-profile.changeUsername("Marco");
-console.log(profile.getInfo()); // "Marco has 300 active hours!"
+//     updatePlayTime(hours) {
+//         this.playTime += hours;
+//     },
 
-profile.updatePlayTime(20);
-console.log(profile.getInfo()); // "Marco has 320 active hours!"
+//     getInfo(){
+// return `${this.username} has ${profile.playTime} active hours!`;
+//     },
+// };
+
+// console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+// profile.changeUsername("Marco");
+// console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+// profile.updatePlayTime(20);
+// console.log(profile.getInfo()); // "Marco has 320 active hours!"
